c665a3019bafbdfd22aa62cbe11162410c814c7a
Kevin Baragona <kmbaragona@gmail.com>
2020-03-04 18:59:52 -0800

    add script to convert keras model to TF checkpoint for use in latentai sdk

--- resnet50/README.md
+++ resnet50/README.md
@@ -4,9 +4,13 @@ Note that Dockerfile is currently for CPU mode
 docker build -t $(cat docker_image_name.txt) .
 
 # Download dataset
+
 ./dev_docker_run ./download_dataset.py
 
 # Train
 
 ./dev_docker_run ./train.py --dataset_path datasets/open_images_10_classes_200/
 
+# Convert Trained Model to TF Checkpoint format
+
+./dev_docker_run ./convert_keras_model_to_checkpoint.py --input_model_path trained_model.h5 --output_model_path converted
---
+++ resnet50/convert_keras_model_to_checkpoint.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import argparse
+import os
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import backend as K
+
+if __name__ == '__main__':
+    # constants
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_model_path',
+        type=str,
+        default=None,
+        required=True,
+        help='Load keras model at path'
+    )
+    parser.add_argument(
+        '--output_model_path',
+        type=str,
+        default='checkpoint',
+        required=False,
+        help='Where to save the converted model.'
+    )
+
+    args = parser.parse_args()
+
+    model = keras.models.load_model(args.input_model_path)
+
+    sess = K.get_session()
+    saver = tf.compat.v1.train.Saver()
+    os.makedirs(args.output_model_path, exist_ok=True)
+    saver.save(sess, os.path.join(args.output_model_path, 'converted'))
