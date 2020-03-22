8c620fa850d91db61256d2e9f9802ef390b20700
Kevin Baragona <kmbaragona@gmail.com>
2020-03-21 19:15:07 -0700

    add evaluate variants script - better usage of subprocess

--- utils/evaluate_variants.py
+++ utils/evaluate_variants.py
@@ -10,36 +10,36 @@ input_names = 'input_1'
 output_names = 'probs/Softmax'
 input_shapes = '1,224,224,3'
 
-subprocess.check_call("rm -rf variants")
-subprocess.check_call("mkdir variants")
+subprocess.check_call(["rm", "-rf", "variants"])
+subprocess.check_call(["mkdir", "variants"])
 
-# Imagenet Baseline TF FP32
-subprocess.check_call("leip evaluate -fw tf2 -in "+input_checkpoint+" --test_path="+dataset_index_file+" --class_names="+class_names_file+" --task=classifier --dataset=custom  --preprocessor "+preprocessor+" --input_shapes "+input_shapes+" --input_names "+input_names+" --output_names "+output_names+"")
-# Imagenet LEIP Compress
+#", "Baseline", "TF", "FP32
+subprocess.check_call(["leip", "evaluate", "-fw", "tf2", "-in", input_checkpoint, "--test_path="+dataset_index_file, "--class_names="+class_names_file, "--task=classifier", "--dataset=custom", "--preprocessor", preprocessor, "--input_shapes", input_shapes, "--input_names", input_names, "--output_names", output_names])
+#", "LEIP", "Compress
 
-subprocess.check_call("leip compress -in "+input_checkpoint+" -q ASYMMETRIC -b 8 -out variants/checkpointCompressed/")
-subprocess.check_call("leip compress -in "+input_checkpoint+" -q POWER_OF_TWO -b 8 -out variants/checkpointCompressedPow2/")
+subprocess.check_call(["leip", "compress", "-in", input_checkpoint, "-q", "ASYMMETRIC", "-b", "8", "-out", "variants/checkpointCompressed/"])
+subprocess.check_call(["leip", "compress", "-in", input_checkpoint, "-q", "POWER_OF_TWO", "-b", "8", "-out", "variants/checkpointCompressedPow2/"])
 
-# Imagenet LEIP TF FP32
-subprocess.check_call("leip evaluate -fw tf2 -in variants/checkpointCompressed/model_save/ --test_path="+dataset_index_file+" --class_names="+class_names_file+" --task=classifier --dataset=custom  --preprocessor "+preprocessor+"  --input_shapes "+input_shapes+" --input_names "+input_names+" --output_names "+output_names+"")
+#", "LEIP", "TF", "FP32
+subprocess.check_call(["leip", "evaluate", "-fw", "tf2", "-in", "variants/checkpointCompressed/model_save/", "--test_path="+dataset_index_file, "--class_names="+class_names_file, "--task=classifier", "--dataset=custom", "--preprocessor", preprocessor, "--input_shapes", input_shapes, "--input_names", input_names, "--output_names", output_names])
 
-# Imagenet Baseline TVM INT8
-subprocess.check_call("rm -rf variants/compiled_tvm_int8")
-subprocess.check_call("mkdir variants/compiled_tvm_int8")
-subprocess.check_call("leip compile -in "+input_checkpoint+" -ishapes "+input_shapes+" -o variants/compiled_tvm_int8/bin --input_types=uint8  --data_type=int8")
-subprocess.check_call("leip evaluate -fw tvm --input_names "+input_names+" --input_types=uint8 -ishapes "+input_shapes+" -in variants/compiled_tvm_int8/bin --test_path="+dataset_index_file+" --class_names="+class_names_file+" --task=classifier --dataset=custom  --preprocessor "+preprocessor)
-# Imagenet Baseline TVM FP32
-subprocess.check_call("rm -rf variants/compiled_tvm_fp32")
-subprocess.check_call("mkdir variants/compiled_tvm_fp32")
-subprocess.check_call("leip compile -in "+input_checkpoint+" -ishapes "+input_shapes+" -o variants/compiled_tvm_fp32/bin --input_types=float32  --data_type=float32")
-subprocess.check_call("leip evaluate -fw tvm --input_names "+input_names+" --input_types=float32 -ishapes "+input_shapes+" -in variants/compiled_tvm_fp32/bin --test_path="+dataset_index_file+" --class_names="+class_names_file+" --task=classifier --dataset=custom  --preprocessor "+preprocessor)
-# Imagenet LEIP TVM INT8
-subprocess.check_call("rm -rf variants/leip_compiled_tvm_int8")
-subprocess.check_call("mkdir variants/leip_compiled_tvm_int8")
-subprocess.check_call("leip compile -in variants/checkpointCompressed/model_save/ -ishapes "+input_shapes+" -o variants/leip_compiled_tvm_int8/bin --input_types=uint8  --data_type=int8")
-subprocess.check_call("leip evaluate -fw tvm --input_names "+input_names+" --input_types=uint8 -ishapes "+input_shapes+" -in variants/leip_compiled_tvm_int8/bin --test_path="+dataset_index_file+" --class_names="+class_names_file+" --task=classifier --dataset=custom  --preprocessor "+preprocessor)
-# Imagenet LEIP TVM FP32
-subprocess.check_call("rm -rf variants/leip_compiled_tvm_fp32")
-subprocess.check_call("mkdir variants/leip_compiled_tvm_fp32")
-subprocess.check_call("leip compile -in variants/checkpointCompressed/model_save/ -ishapes "+input_shapes+" -o variants/leip_compiled_tvm_fp32/bin --input_types=float32  --data_type=float32")
-subprocess.check_call("leip evaluate -fw tvm --input_names "+input_names+" --input_types=float32 -ishapes "+input_shapes+" -in variants/leip_compiled_tvm_fp32/bin --test_path="+dataset_index_file+" --class_names="+class_names_file+" --task=classifier --dataset=custom  --preprocessor "+preprocessor)
+#", "Baseline", "TVM", "INT8
+subprocess.check_call(["rm", "-rf", "variants/compiled_tvm_int8"])
+subprocess.check_call(["mkdir", "variants/compiled_tvm_int8"])
+subprocess.check_call(["leip", "compile", "-in", input_checkpoint, "-ishapes", input_shapes, "-o", "variants/compiled_tvm_int8/bin", "--input_types=uint8", "--data_type=int8"])
+subprocess.check_call(["leip", "evaluate", "-fw", "tvm", "--input_names", input_names, "--input_types=uint8", "-ishapes", input_shapes, "-in", "variants/compiled_tvm_int8/bin", "--test_path="+dataset_index_file, "--class_names="+class_names_file, "--task=classifier", "--dataset=custom", "--preprocessor", preprocessor])
+#", "Baseline", "TVM", "FP32
+subprocess.check_call(["rm", "-rf", "variants/compiled_tvm_fp32"])
+subprocess.check_call(["mkdir", "variants/compiled_tvm_fp32"])
+subprocess.check_call(["leip", "compile", "-in", input_checkpoint, "-ishapes", input_shapes, "-o", "variants/compiled_tvm_fp32/bin", "--input_types=float32", "--data_type=float32"])
+subprocess.check_call(["leip", "evaluate", "-fw", "tvm", "--input_names", input_names, "--input_types=float32", "-ishapes", input_shapes, "-in", "variants/compiled_tvm_fp32/bin", "--test_path="+dataset_index_file, "--class_names="+class_names_file, "--task=classifier", "--dataset=custom", "--preprocessor", preprocessor])
+#", "LEIP", "TVM", "INT8
+subprocess.check_call(["rm", "-rf", "variants/leip_compiled_tvm_int8"])
+subprocess.check_call(["mkdir", "variants/leip_compiled_tvm_int8"])
+subprocess.check_call(["leip", "compile", "-in", "variants/checkpointCompressed/model_save/", "-ishapes", input_shapes, "-o", "variants/leip_compiled_tvm_int8/bin", "--input_types=uint8", "--data_type=int8"])
+subprocess.check_call(["leip", "evaluate", "-fw", "tvm", "--input_names", input_names, "--input_types=uint8", "-ishapes", input_shapes, "-in", "variants/leip_compiled_tvm_int8/bin", "--test_path="+dataset_index_file, "--class_names="+class_names_file, "--task=classifier", "--dataset=custom", "--preprocessor", preprocessor])
+#", "LEIP", "TVM", "FP32
+subprocess.check_call(["rm", "-rf", "variants/leip_compiled_tvm_fp32"])
+subprocess.check_call(["mkdir", "variants/leip_compiled_tvm_fp32"])
+subprocess.check_call(["leip", "compile", "-in", "variants/checkpointCompressed/model_save/", "-ishapes", input_shapes, "-o", "variants/leip_compiled_tvm_fp32/bin", "--input_types=float32", "--data_type=float32"])
+subprocess.check_call(["leip", "evaluate", "-fw", "tvm", "--input_names", input_names, "--input_types=float32", "-ishapes", input_shapes, "-in", "variants/leip_compiled_tvm_fp32/bin", "--test_path="+dataset_index_file, "--class_names="+class_names_file, "--task=classifier", "--dataset=custom", "--preprocessor", preprocessor])
